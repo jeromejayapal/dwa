d0d2cadbabed3354132c7a3b85aa7057b15c8f7e
Jerome Jaypaal <jeromejayapal@fas.harvard.edu>
2012-09-21 20:49:59 -0400

    Homework P1 First Draft

--- index.php
+++
@@ -1,3 +0,0 @@
-Hello World
-
-Simply a test
---
+++ p1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<style type='text/css'>
+	
+</head>
+
+<body>
+
+	<div id='cylinder'>
+		
+		<h1>Jerome Jayapal</h1>
+		
+		<img src='profile.jpg'>
+		
+		<h3>Hello! My name is Jerome Jayapal. I work at Verizon Communications
+		as a Desktop Engineer. I recently graduated but I really love learning new things
+		and wanted to take the chance to learn an aspect of web development that I know
+		very little about...but that is all about to change with this class.</h3>
+		</br>
+		<h3>I decided to go with asmallorange as my web host and I am using their small sharing plan</h3>
+		</br>
+		<h3>I started off using Dreamweaver as my code editor but after all of the negative press from 
+		Professor Susan Buck I decided to switch to Notepad++ :)</h3>
+		</br>
+		<h3>My git repository is http://github.com/jeromejayapal/dwa</h3>
+		
+	</div>
+		
+		
+</body>
+
+</html>
