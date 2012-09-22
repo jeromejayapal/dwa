851ae1f5525e936308c8f9db1f29df2388268422
Jerome Jaypaal <jeromejayapal@fas.harvard.edu>
2012-09-21 21:45:21 -0400

    Homework P1 Final

--- p1/index.php
+++ p1/index.php
@@ -5,9 +5,21 @@
 	<style type='text/css'>
 	
 		#cylinder {
+					width: 900px;
 					margin: auto;
-					background-color: grey;
+					background-color: #E0E0E0;
+					padding: 30px;
 		}
+		
+		img {
+					width: 216px; height: 194px;
+					float: right;
+		}
+		
+		body {
+					font-family: Arial;
+		}
+		
 	</style>
 	
 </head>
@@ -15,25 +27,19 @@
 <body>
 
 	<div id='cylinder'>
-		
-		<h1>Jerome Jayapal</h1>
-		
+				
 		<img src='profile.jpg'>
-		
-		<h3>Hello! My name is Jerome Jayapal. I work at Verizon Communications
-		as a Desktop Engineer. I recently graduated but I really love learning new things
+		<h1>Jerome Jayapal</h1>
+		<p>Hello! My name is Jerome Jayapal. I work at Verizon Communications
+		as a Desktop Engineer. I recently graduated but I love learning
 		and wanted to take the chance to learn an aspect of web development that I know
-		very little about...but that is all about to change with this class.</h3>
-		</br>
-		<h3>I decided to go with asmallorange as my web host and I am using their small sharing plan</h3>
-		</br>
-		<h3>I started off using Dreamweaver as my code editor but after all of the negative press from 
-		Professor Susan Buck I decided to switch to Notepad++ :)</h3>
-		</br>
-		<h3>My git repository is http://github.com/jeromejayapal/dwa</h3>
+		very little about...but that is all about to change with this class.</p>
+		<p>I decided to go with asmallorange as my web host and I am using their small sharing plan</p>
+		<p>I started off using Dreamweaver as my code editor but after all of the negative press from 
+		Professor Susan Buck I decided to switch to Notepad++ :)</p>
+		<p>My git repository is http://github.com/jeromejayapal/dwa</p>
 		
 	</div>
-		
 		
 </body>
 
