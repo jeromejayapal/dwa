86b57b669f71f0641e19d0276f9efc06c9e43b12
Jerome Jayapal <jeromejayapal@gmail.com>
2012-12-23 10:51:21 -0500

    Project 4 Draft 12
    Updated contact page picture links to correct extension (JPG -> jpg)

--- p4.jeromejayapal.com/views/v_index_contact.php
+++ p4.jeromejayapal.com/views/v_index_contact.php
@@ -26,16 +26,16 @@
 <body>	
 	
 	<div id='blog'>		
-			<img class='profile' src="../images/slide-3.JPG">
+			<img class='profile' src="../images/slide-3.jpg">
 			<p>How are we going to keep in touch? Great question! The first way
 			is by visiting this website. Below are links to some other social media applications
 			that I use.</p>
 			<br /><br /><br /><br /><br /><br /><br /><br />
 		<div id="contact">
-			<a href="https://www.facebook.com/jerome.jayapal" target="_blank"><img src="../images/facebook.PNG"  heigth="30" width="30"></a>
-			<a href="http://twitter.com/jeromejayapal" target="_blank"><img src="../images/twitter.PNG" heigth="30" width="30"></a>
-			<a href="https://live.xbox.com/en-US/Profile?GamerTag=TheSticker" target="_blank"><img src="../images/xbox.JPG" heigth="30" width="30"></a>
-			<a href="mailto:jeromejayapal@fas.harvard.edu"><img src="../images/email.JPG" heigth="30" width="30"></a>
+			<a href="https://www.facebook.com/jerome.jayapal" target="_blank"><img src="../images/facebook.png"  heigth="30" width="30"></a>
+			<a href="http://twitter.com/jeromejayapal" target="_blank"><img src="../images/twitter.png" heigth="30" width="30"></a>
+			<a href="https://live.xbox.com/en-US/Profile?GamerTag=TheSticker" target="_blank"><img src="../images/xbox.jpg" heigth="30" width="30"></a>
+			<a href="mailto:jeromejayapal@fas.harvard.edu"><img src="../images/email.jpg" heigth="30" width="30"></a>
 		</div>
 		</div>
 	</div>
